1569fc4121d18f3ebd1b7daf0cbedb4acff54cb0
liushuai <770722922@qq.com>
2022-10-17 11:33:27 +0800

    feat: EnableVerify option for FlatbuffersMsg

--- plugin/FlatbuffersMsg.hpp
+++ plugin/FlatbuffersMsg.hpp
@@ -3,7 +3,7 @@
 
 namespace RpcCore {
 
-template <typename T, size_t InitialSize = 1024>
+template <typename T, size_t InitialSize = 1024, bool EnableVerify = true>
 struct FlatbuffersMsg : RpcCore::Message {
   using TableType = typename T::TableType;
   std::string serialize() const override {
@@ -16,10 +16,12 @@ struct FlatbuffersMsg : RpcCore::Message {
   }
 
   bool deSerialize(const std::string& data) override {
-    flatbuffers::Verifier verifier((uint8_t*)data.data(), data.size());
-    bool ok = verifier.VerifyBuffer<TableType>();
-    if (!ok) {
-      return false;
+    if (EnableVerify) {
+      flatbuffers::Verifier verifier((uint8_t*)data.data(), data.size());
+      bool ok = verifier.VerifyBuffer<TableType>();
+      if (!ok) {
+        return false;
+      }
     }
     flatbuffers::GetRoot<TableType>(data.data())->UnPackTo(&msg);
     return true;
