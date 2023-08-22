df51e0e67c9cee32eb2869351d2947d1a258b275
liushuai <770722922@qq.com>
2023-08-22 21:27:40 +0800

    fix: raw type deserialize

--- src/serialize/raw_type.hpp
+++ src/serialize/raw_type.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <array>
 #include <cstring>
 #include <type_traits>
 
@@ -11,7 +10,8 @@
   }                                                                                                 \
   template <typename T, typename std::enable_if<std::is_same<T, type_raw>::value, int>::type = 0>   \
   inline bool deserialize(const detail::string_view& data, T& t, std::size_t* cost_len = nullptr) { \
-    memcpy(&t, data.data(), type_size);                                                             \
+    t = {};                                                                                         \
+    memcpy(&t, data.data(), detail::min<size_t>(sizeof(t), type_size));                             \
     if (cost_len) {                                                                                 \
       *cost_len = type_size;                                                                        \
     }                                                                                               \
@@ -19,6 +19,13 @@
   }
 
 namespace RpcCore {
+
+namespace detail {
+template <typename T>
+constexpr const T& min(const T& a, const T& b) {
+  return a < b ? a : b;
+}
+}  // namespace detail
 
 RpcCore_DETAIL_DEFINE_RAW_TYPE(bool, 1);
 RpcCore_DETAIL_DEFINE_RAW_TYPE(char, 1);
