d8dff875e7567762f83a5568d83391f68aa6e0eb
liushuai <770722922@qq.com>
2025-01-01 17:33:56 +0800

    feat: release v2.1.0

--- include/rpc_core/version.hpp
+++ include/rpc_core/version.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #define RPC_CORE_VER_MAJOR 2
-#define RPC_CORE_VER_MINOR 0
-#define RPC_CORE_VER_PATCH 3
+#define RPC_CORE_VER_MINOR 1
+#define RPC_CORE_VER_PATCH 0
 
 #define RPC_CORE_TO_VERSION(major, minor, patch) (major * 10000 + minor * 100 + patch)
 #define RPC_CORE_VERSION RPC_CORE_TO_VERSION(RPC_CORE_VER_MAJOR, RPC_CORE_VER_MINOR, RPC_CORE_VER_PATCH)
